0d571e4086d86bb61d5711cf944ef3d7ac600b7b
jsh112 <jshkr1120@gmail.com>
2026-08-07 21:16:17 +0900

    fix: [LAB-12] 순례길 Seed 데이터 중복 삽입 오류 수정

--- src/main/java/com/se_lab/project/repository/PilgrimageRouteRepository.java
+++ src/main/java/com/se_lab/project/repository/PilgrimageRouteRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PilgrimageRouteRepository extends JpaRepository<PilgrimageRoute, Long> {
+public interface PilgrimageRouteRepository
+        extends JpaRepository<PilgrimageRoute, Long> {
+
+    boolean existsByIdentifier(String identifier);
 }
